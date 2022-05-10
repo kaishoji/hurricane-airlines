f7b5b52f61fd17e9d41668c126dc109c6376641f
kai shoji ohsawa <kaishoji.oh@gmail.com>
2022-05-10 13:47:21 -0700

    Added axios dependancy, updated web name

--- hurricane-airlines/src/Register.js
+++ hurricane-airlines/src/Register.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c89d3042c510267c93c527a4731415887387b0b612721ee271399512c48ecfe
-size 6345
+oid sha256:be6ed186e51e6ff52dfee6c27161b1e80d0d91bead22ce52966025eee27f383a
+size 7219
