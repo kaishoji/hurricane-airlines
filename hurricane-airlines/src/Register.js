bfe16974288bc5c54f9d49fee6a896be1e2f2866
kai shoji ohsawa <kaishoji.oh@gmail.com>
2022-05-10 00:50:02 -0700

    Started work on a registration page

---
+++ hurricane-airlines/src/Register.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c89d3042c510267c93c527a4731415887387b0b612721ee271399512c48ecfe
+size 6345
--- hurricane-airlines/src/components/Footer.js
+++ hurricane-airlines/src/components/Footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54ea40e5771be9544c6e47b12912e4e148d820a8c7f9af97a0a2d5669934ff99
-size 3850
+oid sha256:2cff8f821923483bd4babdd21632cec6707eece13aaead01b29fbb42567ae9f4
+size 3876
